a57401f98536b59b7871c653533b3e1cbecc28c9
emilyjohnson <ejohnson7@collegeforcreativestudies.edu>
2018-03-28 17:02:59 -0400

    check

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	// col-1
+for (i = 1; i < 102; i++) {
+  $('#col-1').append('<div class="stripe"></div>')
+}
+
+// col-2
+for (i = 1; i < 101; i++) {
+  $('#col-2').append('<div class="stripe"></div>')
+}
+
+// col-3
+for (i = 1; i < 51; i++) {
+  $('#col-3').append('<div class="stripe"></div>')
+}
+
+});
